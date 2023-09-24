117fd620e9ec549e60fb662082915d259255e0e9
bienculo8282 <139576246+bienculo8282@users.noreply.github.com>
2023-09-24 11:28:51 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ module.exports = {
     return [
       {
         source: '/posts/:slug',
-        destination: 'https://thesenholding.com/:slug',
+        destination: 'https://themedipia.com/:slug',
         has: [
           {
             type: 'header',
